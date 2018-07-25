360bd35dd2b58adb76b86d1350247457edbe9f11
László Bácsi <lackac@lackac.hu>
2018-07-25 15:31:35 +0200

    Update Travis configuration and add badge to README (#61)

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,3 @@
-ExUnit.start(assert_receive_timeout: 200)
+assert_timeout = String.to_integer(System.get_env("ELIXIR_ASSERT_TIMEOUT") || "500")
+
+ExUnit.start(assert_receive_timeout: assert_timeout)
