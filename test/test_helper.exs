fad31d421bb6700a2af976363bb637dc809547bb
László Bácsi <lackac@lackac.hu>
2018-07-29 19:28:00 +0200

    Attempt to fix random failures on CI (#66)

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,3 @@
-assert_timeout = String.to_integer(System.get_env("ELIXIR_ASSERT_TIMEOUT") || "500")
+assert_timeout = String.to_integer(System.get_env("ELIXIR_ASSERT_TIMEOUT") || "1000")
 
 ExUnit.start(assert_receive_timeout: assert_timeout)
